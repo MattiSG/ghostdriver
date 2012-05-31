a6cb32205c88c4f1742dbcedb537a840d0a23908
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-05-31 16:50:22 +0200

    Heavy refactor of GoogleCheese example. Externalized paths management and drivers list to properties list. Stopped using ints to distinguish drivers. Defaulted to computing stats when no specific driver is given. Switched to nanoseconds granularity for stats. Made runaway phantomjs processes killing more resilient.
    (really sorry for such a huge commit, but no stable point was encountered at all during the refactor)

--- examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
+++ examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
@@ -13,172 +13,196 @@ import org.openqa.selenium.remote.UnreachableBrowserException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
+import java.io.FileReader;
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
 
 
 public class GoogleCheese {
-    private static final int USE_GHOSTDRIVER = 0;
-    private static final int USE_FIREFOX_DRIVER = 1;
-    private static final int USE_CHROME_DRIVER = 2;
-    private static final int USE_ALL_AND_MAKE_STATS = 3;
-
+    /** Set of files from which to fetch the properties.
+     */
+    //@{
+    private static final String DRIVERS_LIST_FILE = "drivers.properties"; // list of all drivers and their user-readable description
+    
+    private static final String DRIVERS_PATHS_FILE = "paths.properties"; // paths to all necessary executables; the contents of that file should be edited to reflect your setup
+    //@}
+    
     private static final int STATS_SAMPLING_SIZE = 100;
+    
+    
+    private static Properties availableDrivers;
+    private static Properties paths;
 
-    private static final String PHANTOMJS_PATH = "/Users/idemarin/Workspaces/qt/phantomjs/bin/phantomjs";   //< CHANGEME
-    private static final String GHOSTDRIVER_PATH = "/Users/idemarin/Workspaces/qt/ghostdriver/main.js";     //< CHANGEME
-    private static final String CHROMEDRIVER_PATH = "/Users/idemarin/bin/chromedriver";                     //< CHANGEME
-
-    public static void main( String[] args ) throws MalformedURLException, IOException, InterruptedException {
-        int use = USE_GHOSTDRIVER;
-
-        if (args.length == 1) {
-            if (args[0].toLowerCase().equals("firefox")) {
-                use = USE_FIREFOX_DRIVER;
-            } else if (args[0].toLowerCase().equals("chrome")) {
-                use = USE_CHROME_DRIVER;
-            } else if (args[0].toLowerCase().equals("stats")) {
-                use = USE_ALL_AND_MAKE_STATS;
-            }
+    
+    private static void init() {
+        try {
+            availableDrivers =  new Properties();
+            availableDrivers.load(new FileReader(DRIVERS_LIST_FILE));
+        } catch (Exception e) {
+            throw new RuntimeException("Error while trying to load drivers list file (" + DRIVERS_LIST_FILE + ")", e);
         }
-
-        if (use == USE_ALL_AND_MAKE_STATS) {
-            // Let's make some stats
-            long stats[][] = new long[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-            for (int driverType : new int[]{0, 1, 2}) {
-                long sample[] = null;
-
-                // Sum up samples
-                for (int i = 0; i < STATS_SAMPLING_SIZE; ++i) {
-                    sample = runTest(driverType);
-                    stats[driverType][0] += sample[0];
-                    stats[driverType][1] += sample[1];
-                    stats[driverType][2] += sample[2];
-                }
-
-                // Normalize
-                stats[driverType][0] /= STATS_SAMPLING_SIZE;
-                stats[driverType][1] /= STATS_SAMPLING_SIZE;
-                stats[driverType][2] /= STATS_SAMPLING_SIZE;
-            }
-
-            for (int driverType : new int[]{0, 1, 2}) {
-                System.out.println("");
-                System.out.println("--------------------------------------");
-                System.out.println("*** STATS for Driver Type: "+driverName(driverType));
-                System.out.println("*** Average Driver Start Time over "+STATS_SAMPLING_SIZE+" samples: "+stats[driverType][0]);
-                System.out.println("*** Average Test Execution Time over "+STATS_SAMPLING_SIZE+" samples: "+stats[driverType][1]);
-                System.out.println("*** Total Execution Time over "+STATS_SAMPLING_SIZE+" samples: "+stats[driverType][2]);
-                System.out.println("--------------------------------------");
-                System.out.println("");
-            }
+        
+        try {
+            paths = new Properties();
+            paths.load(new FileReader(DRIVERS_PATHS_FILE));
+        } catch (Exception e) {
+            throw new RuntimeException("Error while trying to load executable paths file (" + DRIVERS_PATHS_FILE + ")", e);
+        }
+    }
+    
+    public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException {
+        init();
+        
+        if (args.length == 0) {
+            System.out.println("Testing all drivers and computing stats on them\n"
+                               + "-----------------------------------------------\n"
+                               + "If you simply want to test one driver, provide its name as argument.\n"
+                               + "You can see and edit possible drivers in the .properties files along this example file.");
         } else {
-            // Just test 1 browser (boring)
-            GoogleCheese.runTest(use);
+            runTestWithDriver(args[0].toLowerCase());
         }
 
         System.exit(0);
     }
+    
+    public static long[][] runStats() {
+        long stats[][] = new long[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        byte driverCount = 0;
 
-    public static String driverName(int driverType) {
-        if (driverType == USE_GHOSTDRIVER) return "GhostDriver + PhantomJS";
-        if (driverType == USE_FIREFOX_DRIVER) return "FirefoxDriver + Firefox";
-        if (driverType == USE_CHROME_DRIVER) return "ChromeDriver + Chrome";
-        return "UNKNOWN";
+        for (String currentDriver : availableDrivers.stringPropertyNames()) {
+            long sample[];
+            
+            try {
+                // Sum up samples
+                for (int i = 0; i < STATS_SAMPLING_SIZE; ++i) {
+                    sample = runTestWithDriver(currentDriver);
+                    stats[driverCount][0] += sample[0];
+                    stats[driverCount][1] += sample[1];
+                    stats[driverCount][2] += sample[2];
+                }
+                
+                // Normalize
+                stats[driverCount][0] /= STATS_SAMPLING_SIZE;
+                stats[driverCount][1] /= STATS_SAMPLING_SIZE;
+                stats[driverCount][2] /= STATS_SAMPLING_SIZE;
+                
+                // Display results
+                System.out.println("");
+                System.out.println("--------------------------------------");
+                System.out.println("*** STATS for driver '" + currentDriver + "', averaged over " + STATS_SAMPLING_SIZE + " runs:");
+                System.out.println("*** Driver start:\t" + (stats[driverCount][0] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("*** Test execution:\t" + (stats[driverCount][1] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("*** Total execution:\t" + (stats[driverCount][2] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("--------------------------------------");
+                System.out.println("");
+            } catch (Exception e) {
+                System.err.println("Woops, an error occured while testing driver '" + currentDriver + "'!");
+                e.printStackTrace();
+            }
+            
+            // Go to next driver
+            driverCount++;
+        }
+        
+        return stats;
     }
 
-    public static long[] runTest(int driverType) throws MalformedURLException, IOException, InterruptedException {
-        long timeToStartDriver = 0;
-        long timeToTest = 0;
-        long totalTime = 0;
-
-        // Start the clock
-        Date start = new Date();
-
+    /**
+     *@return   Array of nanoseconds durations: {timeToStartDriver, timeToTest, totalTime}
+     */
+    public static long[] runTestWithDriver(String driverName) throws MalformedURLException, IOException, InterruptedException {
+        long timeToStartDriver,
+             timeToTest,
+             totalTime;
+        
         // Ask for a JavaScript-enabled browser
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setJavascriptEnabled(true);
-
+        
         WebDriver driver = null;
         Capabilities actualCapabilities = null;
         Process phantomjsProcess = null;
 
-        switch (driverType) {
-            case USE_FIREFOX_DRIVER: {
-                System.out.println("*** USING FIREFOX DRIVER ***");
+        // Start the clock
+        long startTime = System.nanoTime();
 
-                driver = new FirefoxDriver(capabilities);
+        if (driverName.equals("firefox")) {
+            System.out.println("*** USING FIREFOX DRIVER ***");
 
-                actualCapabilities = ((FirefoxDriver) driver).getCapabilities();
-                break;
+            driver = new FirefoxDriver(capabilities);
+
+            actualCapabilities = ((FirefoxDriver) driver).getCapabilities();
+        } else if (driverName.equals("chrome")) {
+            System.out.println("*** USING CHROME DRIVER ***");
+
+            System.setProperty("webdriver.chrome.driver", paths.getProperty("chromedriver"));
+            driver = new ChromeDriver();
+
+            actualCapabilities = ((ChromeDriver) driver).getCapabilities();
+        } else if (driverName.equals("ghost")) {
+            System.out.println("*** USING GHOSTDRIVER ***");
+
+            phantomjsProcess = Runtime.getRuntime().exec(paths.getProperty("phantomjs") + ' ' + paths.getProperty("ghostdriver"));
+
+            BufferedReader phantomOutputReader = new BufferedReader(new InputStreamReader(phantomjsProcess.getInputStream()));
+            while (phantomOutputReader.readLine () == null) { } //< As soon as Ghostdriver outputs something, we can continue
+            phantomOutputReader.close();
+
+            try {
+                driver = new RemoteWebDriver(new URL("http://localhost:8080"), capabilities);
+                actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+            } catch (Exception e) {
+                // Ensure we don't leave zombies around...
+                phantomjsProcess.destroy();
             }
-            case USE_CHROME_DRIVER: {
-                System.out.println("*** USING CHROME DRIVER ***");
-                System.out.println("NOTICE: This is currently manually set to a Mac path, and to a specific home directory. CHANGE IT!");
-
-                // Setting property to change where the "chromedriver" executable is expected to be
-                System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_PATH); // CHANGEME
-                driver = new ChromeDriver();
-
-                actualCapabilities = ((ChromeDriver) driver).getCapabilities();
-                break;
-            }
-            case USE_GHOSTDRIVER: default: {
-                System.out.println("*** USING GHOSTDRIVER ***");
-
-                phantomjsProcess = Runtime.getRuntime().exec(PHANTOMJS_PATH + ' ' + GHOSTDRIVER_PATH);
-
-                BufferedReader phantomOutputReader = new BufferedReader (new InputStreamReader(phantomjsProcess.getInputStream()));
-                while (phantomOutputReader.readLine () == null) { } //< As soon as Ghostdriver outputs something, we can continue
-                phantomOutputReader.close();
-
-                try {
-                    driver = new RemoteWebDriver(new URL("http://localhost:8080"), capabilities);
-                    actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
-                } catch (UnreachableBrowserException e) {
-                    // Ensure we don't leave zombies around...
-                    phantomjsProcess.destroy();
-                }
-                break;
-            }
+        } else {
+            System.err.println("Oops, driver '" + driverName + "' is unknown  :/");
+            System.err.println("Take a look at the '" + DRIVERS_LIST_FILE + "' for a list of available drivers.");
         }
-        timeToStartDriver = new Date().getTime() - start.getTime();
-        System.out.println("Driver started in (ms): " + timeToStartDriver);
+        
+        timeToStartDriver = System.nanoTime() - startTime;
+        System.out.println("Driver started in (ns): " + timeToStartDriver);
+        
+        runTestOn(driver);
+        
+        if (phantomjsProcess != null)
+            phantomjsProcess.destroy();
 
+        totalTime = System.nanoTime() - startTime;
+        timeToTest = totalTime - timeToStartDriver;
+        System.out.println("Test done in (ns): " + timeToTest);
+        System.out.println("Time elapsed (ns): " + totalTime);
+
+        return new long[]{timeToStartDriver, timeToTest, totalTime};
+    }
+    
+    private static void runTestOn(WebDriver driver) {
         // And now use this to visit Google
         System.out.println("Loading 'http://www.google.com'...");
         driver.get("http://www.google.com");
         System.out.println("Loaded. Current URL is: '" + driver.getCurrentUrl() + "'");
-
+        
         // Find the text input element by its name
         System.out.println("Finding an Element via [name='q']...");
         WebElement element = driver.findElement(By.name("q"));
         System.out.println("Found.");
-
+        
         // Enter something to search for
         System.out.println("Sending keys 'Cheese!...'");
         element.sendKeys("Cheese!");
         System.out.println("Sent.");
-
+        
         // Now submit the form. WebDriver will find the form for us from the element
         System.out.println("Submitting Element...");
         element.submit();
         System.out.println("Submitted.");
-
+        
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
         driver.close();
-        if (phantomjsProcess != null) {
-            phantomjsProcess.destroy();
-        }
-
-        totalTime = new Date().getTime() - start.getTime();
-        timeToTest = totalTime - timeToStartDriver;
-        System.out.println("Test done in (ms): " + timeToTest);
-        System.out.println("Time elapsed (ms): " + totalTime);
-
-        return new long[]{timeToStartDriver, timeToTest, totalTime};
     }
 }
