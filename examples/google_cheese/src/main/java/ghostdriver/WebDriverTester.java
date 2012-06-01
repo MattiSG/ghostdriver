7f74c0b237721822af8ffbe44c3f40f1f9b8b833
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-01 11:02:09 +0200

    Completely decoupled stats management CLI&testing.

---
+++ examples/google_cheese/src/main/java/ghostdriver/WebDriverTester.java
@@ -0,0 +1,236 @@
+package ghostdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.FileReader;
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public abstract class WebDriverTester {
+    
+    /** Port used by GhostDriver.
+     */
+    public static int port = 8080;
+    
+    
+    /** The desired capabilities for all drivers.
+     */
+    protected DesiredCapabilities desiredCapabilities;
+    
+    /** List of all drivers mapped to their user-readable description
+     */
+    protected static Properties availableDrivers;
+    
+    /** Paths to all necessary executables.
+     */
+    protected static Properties paths;
+
+    
+    /** Set of files from which to fetch the properties.
+     */
+    //@{
+    private static final String DRIVERS_LIST_FILE = "drivers.properties"; // list of all drivers and their user-readable description
+    
+    private static final String DRIVERS_PATHS_FILE = "paths.properties"; // paths to all necessary executables; the contents of that file should be edited to reflect your setup
+    //@}
+    
+    private static final int STATS_SAMPLING_SIZE = 100;
+    
+    /** Some drivers might need to start another process.
+    */
+    private Process serverProcess;
+
+    
+    private void init() {
+        try {
+            availableDrivers =  new Properties();
+            availableDrivers.load(new FileReader(DRIVERS_LIST_FILE));
+        } catch (Exception e) {
+            throw new RuntimeException("Error while trying to load drivers list file (" + DRIVERS_LIST_FILE + ")", e);
+        }
+        
+        try {
+            paths = new Properties();
+            paths.load(new FileReader(DRIVERS_PATHS_FILE));
+        } catch (Exception e) {
+            throw new RuntimeException("Error while trying to load executable paths file (" + DRIVERS_PATHS_FILE + ")", e);
+        }
+        
+        desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setJavascriptEnabled(true);
+    }
+    
+    /**
+     * Usage: [driverName [port]]
+     * Where `diverName` is one of the drivers listed in the `DRIVERS_LIST_FILE` properties file.
+     * If `driverName` refers to GhostDriver, then the optional `port` argument may be used to override the default.
+     */
+    protected WebDriverTester(String[] args) {
+        init();
+        
+        if (args.length == 0) {
+            System.out.println("Testing all drivers and computing stats on them\n"
+                               + "-----------------------------------------------\n"
+                               + "If you simply want to test one driver, provide its name as argument.\n"
+                               + "You can see and edit possible drivers in the .properties files along this example file.");
+            runStats();
+            
+        } else {
+            timeTestWithDriver(args[0].toLowerCase());
+            
+            if (args.length > 1)
+                port = Integer.parseInt(args[1]);
+        }
+
+        System.exit(0);
+    }
+    
+    private long[][] runStats() {
+        long stats[][] = new long[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        byte driverCount = 0;
+
+        for (String currentDriver : availableDrivers.stringPropertyNames()) {
+            long sample[];
+            
+            try {
+                // Sum up samples
+                for (int i = 0; i < STATS_SAMPLING_SIZE; ++i) {
+                    sample = timeTestWithDriver(currentDriver);
+                    stats[driverCount][0] += sample[0];
+                    stats[driverCount][1] += sample[1];
+                    stats[driverCount][2] += sample[2];
+                }
+                
+                // Normalize
+                stats[driverCount][0] /= STATS_SAMPLING_SIZE;
+                stats[driverCount][1] /= STATS_SAMPLING_SIZE;
+                stats[driverCount][2] /= STATS_SAMPLING_SIZE;
+                
+                // Display results
+                System.out.println("");
+                System.out.println("--------------------------------------");
+                System.out.println("*** STATS for driver '" + currentDriver + "', averaged over " + STATS_SAMPLING_SIZE + " runs:");
+                System.out.println("*** Driver start:\t" + (stats[driverCount][0] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("*** Test execution:\t" + (stats[driverCount][1] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("*** Total execution:\t" + (stats[driverCount][2] / 10E6) + " ms"); // divide to obtain ms from ns
+                System.out.println("--------------------------------------");
+                System.out.println("");
+            } catch (Exception e) {
+                System.err.println("Woops, an error occured while testing driver '" + currentDriver + "'!");
+                e.printStackTrace();
+            }
+            
+            // Go to next driver
+            driverCount++;
+        }
+        
+        return stats;
+    }
+
+    /**
+     *@return   Array of nanoseconds durations: {timeToStartDriver, timeToTest, totalTime}, or `null` if the driver is not known.
+     */
+    protected long[] timeTestWithDriver(String driverName) {
+        String driverDescription = availableDrivers.getProperty(driverName);
+                
+        System.out.println("\n\n**********************\n"
+                           + "Using " + driverDescription + "\n"
+                           + "**********************");
+        
+        WebDriver driver;
+        
+        long timeToStartDriver,
+             timeToTest,
+             totalTime;
+        
+        // Start the clock
+        long startTime = System.nanoTime();
+        
+
+        if (driverName.equals("firefox")) {
+            driver = setupFirefoxDriver();
+        } else if (driverName.equals("chrome")) {
+            driver = setupChromeDriver();
+        } else if (driverName.equals("ghost")) {
+            driver = setupGhostDriver();
+        } else {
+            System.err.println("Oops, driver '" + driverName + "' is unknown  :/");
+            System.err.println("Take a look at the '" + DRIVERS_LIST_FILE + "' for a list of available drivers.");
+            
+            return null;	
+        }
+        
+        timeToStartDriver = System.nanoTime() - startTime;
+        System.out.println("Driver started in (ns): " + timeToStartDriver);
+        
+        try {
+            runTestOn(driver);
+        } finally {
+            cleanup();
+        }
+
+        totalTime = System.nanoTime() - startTime;
+        timeToTest = totalTime - timeToStartDriver;
+        System.out.println("Test done in (ns): " + timeToTest);
+        System.out.println("Time elapsed (ns): " + totalTime);
+
+        return new long[]{timeToStartDriver, timeToTest, totalTime};
+    }
+    
+    protected WebDriver setupFirefoxDriver() {
+        System.setProperty("webdriver.firefox.bin", paths.getProperty("firefox"));
+        WebDriver driver = new FirefoxDriver(desiredCapabilities);
+        
+        return driver;
+    }
+    
+    protected WebDriver setupChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", paths.getProperty("chromedriver"));
+        WebDriver driver = new ChromeDriver();
+        
+        return driver;
+    }
+    
+    protected WebDriver setupGhostDriver() {
+        WebDriver driver;
+        try {
+	        serverProcess = Runtime.getRuntime().exec(paths.getProperty("phantomjs") + ' ' + paths.getProperty("ghostdriver"));
+	        
+	        BufferedReader phantomOutputReader = new BufferedReader(new InputStreamReader(serverProcess.getInputStream()));
+	        while (phantomOutputReader.readLine () == null) { } //< As soon as Ghostdriver outputs something, we can continue
+	        phantomOutputReader.close();
+	        
+    	    System.out.println("Remote WebDriver server port: " + port);    
+	        driver = new RemoteWebDriver(new URL("http://localhost:" + port), desiredCapabilities);
+        } catch (Exception e) {
+        	cleanup();
+        	throw new RuntimeException(e);
+        }
+                
+        return driver;
+    }
+    
+    protected void cleanup() {
+        // Ensure we don't leave zombies around...
+        if (serverProcess != null)
+            serverProcess.destroy();
+    }
+    
+    protected abstract void runTestOn(WebDriver driver);
+}
